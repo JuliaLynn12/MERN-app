0937dd3d15c59544ec9c49f5644456616098355a
Julia <julialynn396@gmail.com>
2021-05-25 14:50:48 -0500

    Installing Nodemon

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 app = express()
 
-app.get('/'(req, res)=> {
-  res.send hello
+app.get('/', (req, res)=> {
+  res.send('hello')
+})
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT, ()=>{
+  console.log(`Listening on Port: ${PORT}`)
 })
